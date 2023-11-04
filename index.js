9c03599278425d52b1e16922f00ff7d14171e002
kapil kant <kapilkant471@gmail.com>
2023-11-04 17:56:50 +0530

    creating car-sharing app

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from 'axios';
+import path from "path";
+import bodyParser from "body-parser";
+
+const app=express();
+const port=3000;
+
+app.set('view engine', 'ejs');1
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {  
+      res.render("home.ejs");
+})
+
+app.listen(port,()=>{
+      console.log(`app is listining on the port ${port}`);
+})
